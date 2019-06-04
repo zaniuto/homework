370771af3fed35f22b8dee9906839dfea986a543
Maciej Kwiatkowski <maciej.kwiatkowski92@gmail.com>
2019-06-04 20:34:33 +0200

    Zadanie day 4.

--- day_4/zad_1.py
+++ day_4/zad_1.py
@@ -3,12 +3,19 @@ a = "Zażółć gęślą jaźń."
 
 # vowels = ['a', 'o', 'i', 'e', 'u', 'y', 'ą', 'ę', 'ó']
 vowels = 'aeiouyąęó'
-found_vowels = []
+# found_vowels = []
+found_vowels_2 = []
 
-ii = 0
-a_len = len(a)
-while ii < a_len:
-    if a[ii] in vowels:
-        found_vowels.append(a[ii])
-    ii += 1
-print(found_vowels)
+# ii = 0
+# a_len = len(a)
+# while ii < a_len:
+for elem in a:
+    # elem == a[ii]
+    if elem in vowels:
+        found_vowels_2.append(elem)
+    # if a[ii] in vowels:
+    #     found_vowels.append(a[ii])
+    # ii += 1
+# print(found_vowels)
+print(found_vowels_2)
+# print(found_vowels == found_vowels_2)
