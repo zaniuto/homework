6dd364a21ff1670db95201e335f00002c20029c4
Maciej Kwiatkowski <maciej.kwiatkowski92@gmail.com>
2019-06-06 19:00:56 +0200

    Day 3 homework solution added. Fixes for day 5.

---
+++ day_5/ex_funkcja_inpyt.py
@@ -0,0 +1,15 @@
+
+# def funkcja_2(x, y):
+#     return x + y
+#
+# x = input('Podaj wartość: ')
+#
+# print(funkcja_2(int(x), 2))
+
+def funkcja_2(x, y):
+    print(f'x: {x}, y: {y}')
+
+# print(funkcja_2(1))
+
+x= (1,2)
+print(funkcja_2(x, 4))
---
+++ day_5/ex_funkcja_kolejnosc.py
@@ -0,0 +1,6 @@
+def funkcja_2(x, y):
+    print(f'x: {x}, y: {y}')
+
+x= (1,2)
+z = funkcja_2(4, x)
+print(z)
